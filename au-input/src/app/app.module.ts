d7a525bd2f0a00660135f97fab456ea66c4d088f
Allen Wang <allen.wang2017@yahoo.com>
2020-04-27 21:43:02 -0400

    comments needed

--- au-input/src/app/app.module.ts
+++ au-input/src/app/app.module.ts
@@ -6,8 +6,8 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { AuInputModule } from './lib/au-input-module';
 import { AuMdInputComponent } from './lib/au-md-input/au-md-input.component';
-// import { AuFaInputComponent } from './lib/au-fa-input/au-fa-input.component';
-// import { InputRefDirective } from './lib/common/input-ref.directive';
+// import { AuFaInputComponent } from './lib/au-fa-input/au-fa-input.component'; // being put in AuInputModule
+// import { InputRefDirective } from './lib/common/input-ref.directive'; // being put in AuInputModule
 
 @NgModule({
   declarations: [
